deb162050d42adb1a4b4036c47a5e0dbce4c1cc6
Andre Castellanos <acastellanosaldama@gmail.com>
2020-12-27 22:53:54 -0600

    Added classes to DB tables

---
+++ Dominio/Curso.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class Curso
+    {
+        public int CursoId { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public byte[] FotoPortada { get; set; }
+        public Precio Precio { get; set; }
+        public ICollection<Comentario> ComentarioLista { get; set; }
+        public ICollection<CursoInstructor> InstructoresLista { get; set; }
+    }
+}
