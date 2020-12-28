3b3ad0195a3b03fe5c07a9628a59d4827e735707
Andre Castellanos <acastellanosaldama@gmail.com>
2020-12-27 21:30:10 -0600

    added middleware

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,15 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aplicacion.Cursos;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Persistencia;
 
 namespace WebAPI
 {
@@ -26,7 +30,10 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<CursosOnlineContext>(opt => {
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddMediatR(typeof(Consulta.Handler).Assembly);
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
