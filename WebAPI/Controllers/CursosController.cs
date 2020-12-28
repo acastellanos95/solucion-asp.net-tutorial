ff82ebaa0502fb4dc3b4fbe6cb5965e141faf36f
Andre Castellanos <acastellanosaldama@gmail.com>
2020-12-28 14:08:52 -0600

    created route control for creating, updating and removing in Cursos route

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -17,16 +17,40 @@ namespace WebAPI.Controllers
         {
             _mediator = mediator;
         }
+
         [HttpGet]
         public async Task<ActionResult<List<Curso>>> Get()
         {
             return await _mediator.Send(new Consulta.ListaCursos());
         }
+
         // http://localhost:5000/api/Cursos/{id}
         [HttpGet("{id}")]
         public async Task<ActionResult<Curso>> GetById(int id)
         {
             return await _mediator.Send(new ConsultaId.CursoUnico{Id=id});
+        }
+
+        // http://localhost:5000/api/Cursos
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.NuevoCursoRequest curso)
+        {
+            return await _mediator.Send(curso);
+        }
+
+        // http://localhost:5000/api/Cursos/{id}
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Actualizar(int id, Editar.EditarCursoRequest curso)
+        {
+            curso.CursoId = id;
+            return await _mediator.Send(curso);
+        }
+
+        // http://localhost:5000/api/Cursos/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Eliminar(int id)
+        {
+            return await _mediator.Send(new Eliminar.EliminarCursoRequest{CursoId=id});
         }
     }
 }
