c8cda7810677e32d5b94a6889eba2f9230e5b1a0
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-02 14:07:42 -0600

    update to Mediator instead of _mediator

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -10,32 +10,27 @@ namespace WebAPI.Controllers
     // http://localhost:5000/api/Cursos
     [Route("api/[controller]")]
     [ApiController]
-    public class CursosController : ControllerBase
+    public class CursosController : BaseController
     {
-        private readonly IMediator _mediator;
-        public CursosController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Curso>>> Get()
         {
-            return await _mediator.Send(new Consulta.ListaCursos());
+            return await Mediator.Send(new Consulta.ListaCursos());
         }
 
         // http://localhost:5000/api/Cursos/{id}
         [HttpGet("{id}")]
         public async Task<ActionResult<Curso>> GetById(int id)
         {
-            return await _mediator.Send(new ConsultaId.CursoUnico{Id=id});
+            return await Mediator.Send(new ConsultaId.CursoUnico{Id=id});
         }
 
         // http://localhost:5000/api/Cursos
         [HttpPost]
         public async Task<ActionResult<Unit>> Crear(Nuevo.NuevoCursoRequest curso)
         {
-            return await _mediator.Send(curso);
+            return await Mediator.Send(curso);
         }
 
         // http://localhost:5000/api/Cursos/{id}
@@ -43,14 +38,14 @@ namespace WebAPI.Controllers
         public async Task<ActionResult<Unit>> Actualizar(int id, Editar.EditarCursoRequest curso)
         {
             curso.CursoId = id;
-            return await _mediator.Send(curso);
+            return await Mediator.Send(curso);
         }
 
         // http://localhost:5000/api/Cursos/{id}
         [HttpDelete("{id}")]
         public async Task<ActionResult<Unit>> Eliminar(int id)
         {
-            return await _mediator.Send(new Eliminar.EliminarCursoRequest{CursoId=id});
+            return await Mediator.Send(new Eliminar.EliminarCursoRequest{CursoId=id});
         }
     }
 }
