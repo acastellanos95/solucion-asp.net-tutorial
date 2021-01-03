ec8f561ce5c93eef08b5e6499db0a8b45cf93ca3
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-02 19:47:41 -0600

    added register endpoint and allowed anonymous

--- WebAPI/Controllers/UserController.cs
+++ WebAPI/Controllers/UserController.cs
@@ -2,15 +2,24 @@ using System;
 using System.Threading.Tasks;
 using Aplicacion.Seguridad;
 using Dominio;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
         //http://localhost:5000/api/User/login
         [HttpPost("login")]
         public async Task<ActionResult<UserData>> UserLogin([FromBody]Login.LoginRequest parametros)
+        {
+            return await Mediator.Send(parametros);
+        }
+
+        //http://localhost:5000/api/User/register
+        [HttpPost("register")]
+        public async Task<ActionResult<UserData>> UserRegister([FromBody] Register.RegisterRequest parametros)
         {
             return await Mediator.Send(parametros);
         }
