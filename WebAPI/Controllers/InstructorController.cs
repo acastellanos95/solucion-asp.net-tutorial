08e1d817fd5d3bb7a0b1e76fe5969966cc6ed7ac
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-06 13:00:19 -0600

    getbyid, edit and delete instructor controller done!

--- WebAPI/Controllers/InstructorController.cs
+++ WebAPI/Controllers/InstructorController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Aplicacion.Instructores;
@@ -16,11 +17,33 @@ namespace WebAPI.Controllers
       return await Mediator.Send(new Consulta.Lista());
     }
 
+    // http://localhost:5000/api/Instructor/id
+    [HttpGet("{id}")]
+    public async Task<ActionResult<InstructorModel>> GetByIdInstructor(Guid id)
+    {
+      return await Mediator.Send(new ConsultaId.ConsultaIdInstructorRequest{InstructorId = id});
+    }
+
     // http://localhost:5000/api/Instructor
     [HttpPost]
     public async Task<ActionResult<Unit>> NewInstructor([FromBody] Nuevo.NuevoInstructorRequest request)
     {
       return await Mediator.Send(request);
+    }
+
+    // http://localhost:5000/api/Instructor/id
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Unit>> EditInstructor(Guid id, [FromBody] Editar.EditarInstructorRequest request)
+    {
+      request.InstructorId = id;
+      return await Mediator.Send(request);
+    }
+
+    // http://localhost:5000/api/Instructor/id
+    [HttpDelete("{id}")]
+    public async Task<ActionResult<Unit>> DeleteInstructor(Guid id)
+    {
+      return await Mediator.Send(new Eliminar.EliminarInstructorRequest{InstructorId = id});
     }
   }
 }
