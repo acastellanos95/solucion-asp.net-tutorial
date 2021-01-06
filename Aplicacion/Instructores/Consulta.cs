b586b449286cfd706766ada6e00c740a0b40508b
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-06 13:02:15 -0600

    prettier used

--- Aplicacion/Instructores/Consulta.cs
+++ Aplicacion/Instructores/Consulta.cs
@@ -6,22 +6,22 @@ using Persistencia.DapperConnection.Instructor;
 
 namespace Aplicacion.Instructores
 {
-    public class Consulta
-    {
-        public class Lista : IRequest<List<InstructorModel>>
-        {        }
+  public class Consulta
+  {
+    public class Lista : IRequest<List<InstructorModel>>
+    { }
 
     public class ListaHandler : IRequestHandler<Lista, List<InstructorModel>>
     {
-        private readonly IInstructor _instructorRepositorio;
-        public ListaHandler(IInstructor instructorRepositorio)
-        {
-            _instructorRepositorio = instructorRepositorio;
-        }
-        public async Task<List<InstructorModel>> Handle(Lista request, CancellationToken cancellationToken)
-        {
-            return await _instructorRepositorio.GetInstructorList();
-        }
+      private readonly IInstructor _instructorRepositorio;
+      public ListaHandler(IInstructor instructorRepositorio)
+      {
+        _instructorRepositorio = instructorRepositorio;
+      }
+      public async Task<List<InstructorModel>> Handle(Lista request, CancellationToken cancellationToken)
+      {
+        return await _instructorRepositorio.GetInstructorList();
+      }
     }
   }
 }
