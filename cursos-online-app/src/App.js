ea9eac254c3c21a04e13da436b312ab6fa0313c3
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-09 17:55:18 -0600

    insert RegisterUser component into App

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MuithemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import theme from "./Theme/Theme";
+import RegisterUser from "./Components/Security/RegisterUser";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuithemeProvider theme={theme}>
+        <RegisterUser />
+    </MuithemeProvider>
   );
 }
 
