6fe4ba83b62015603bc122ed2eb9de6c91d5a128
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-09 10:44:23 -0600

    formatted

--- Aplicacion/Seguridad/CurrentUser.cs
+++ Aplicacion/Seguridad/CurrentUser.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Aplicacion.Contratos;
@@ -7,37 +8,38 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Aplicacion.Seguridad
 {
-    public class CurrentUser
+  public class CurrentUser
+  {
+    public class CurrentUserRequest : IRequest<UserData>
     {
-      public class CurrentUserRequest : IRequest<UserData>
-      {
 
+    }
+
+    public class CurrentUserHandler : IRequestHandler<CurrentUserRequest, UserData>
+    {
+      private readonly UserManager<User> _userManager;
+      private readonly IJwtGenerator _jwtGenerator;
+      private readonly IUserSession _userSession;
+      public CurrentUserHandler(UserManager<User> userManager, IJwtGenerator jwtGenerator, IUserSession userSession)
+      {
+        _userManager = userManager;
+        _jwtGenerator = jwtGenerator;
+        _userSession = userSession;
       }
 
-      public class CurrentUserHandler : IRequestHandler<CurrentUserRequest, UserData>
+      public async Task<UserData> Handle(CurrentUserRequest request, CancellationToken cancellationToken)
       {
-        private readonly UserManager<User> _userManager;
-        private readonly IJwtGenerator _jwtGenerator;
-        private readonly IUserSession _userSession;
-        public CurrentUserHandler(UserManager<User> userManager, IJwtGenerator jwtGenerator, IUserSession userSession)
+        var user = await _userManager.FindByNameAsync(_userSession.ObtainUserSession());
+        var listaRoles = await _userManager.GetRolesAsync(user);
+        return new UserData
         {
-          _userManager = userManager;
-          _jwtGenerator = jwtGenerator;
-          _userSession = userSession;
-        }
-
-        public async Task<UserData> Handle(CurrentUserRequest request, CancellationToken cancellationToken)
-        {
-          var user = await _userManager.FindByNameAsync(_userSession.ObtainUserSession());
-          return new UserData
-          {
-            NombreCompleto = user.NombreCompleto,
-            Username = user.UserName,
-            Token = _jwtGenerator.CrearToken(user),
-            Imagen = null,
-            Email = user.Email
-          };
-        }
+          NombreCompleto = user.NombreCompleto,
+          Username = user.UserName,
+          Token = _jwtGenerator.CrearToken(user, new List<string>(listaRoles)),
+          Imagen = null,
+          Email = user.Email
+        };
       }
+    }
   }
 }
