c71cdbf075f4c0a4b6ee00a8a7d0282045fbee4f
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-11 10:56:40 -0600

    refactored style json

--- cursos-online-app/src/App.js
+++ cursos-online-app/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import MuithemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import {ThemeProvider as MuithemeProvider} from "@material-ui/core/styles";
 import theme from "./Theme/Theme";
 import RegisterUser from "./Components/Security/RegisterUser";
+import LoginUser from "./Components/Security/LoginUser";
+import CreateUserProfile from "./Components/Security/CreateUserProfile";
 
 function App() {
   return (
     <MuithemeProvider theme={theme}>
         <RegisterUser />
+        <LoginUser />
+        <CreateUserProfile />
     </MuithemeProvider>
   );
 }
