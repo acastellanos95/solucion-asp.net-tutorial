8367fe574b5072d3454917f376032a81b242f023
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-12 23:26:11 -0600

    NavBar components

---
+++ cursos-online-app/src/Components/Navegation/Bar/BarSession.js
@@ -0,0 +1,57 @@
+import {
+  Avatar,
+  Button,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import reactImg from "../../../logo.svg";
+
+const useStyles = makeStyles((theme) => ({
+  seccionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  seccionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  avatarSize: {
+    width: 40,
+    height: 40,
+  },
+}));
+
+const BarSession = () => {
+  const classes = useStyles();
+  return (
+    <Toolbar>
+      <IconButton color="inherit">
+        <i className="material-icons">menu</i>
+      </IconButton>
+      <Typography variant="h6">Cursos Online</Typography>
+      <div className={classes.grow}></div>
+      <div className={classes.seccionDesktop}>
+        <Button color="inherit">Salir</Button>
+        <Button color="inherit">{"Nombre de usuario"}</Button>
+        <Avatar src={reactImg} />
+      </div>
+      <div className={classes.seccionMobile}>
+        <IconButton color="inherit">
+          <i className="material-icons">more_vert</i>
+        </IconButton>
+      </div>
+    </Toolbar>
+  );
+};
+
+export default BarSession;
