2125a30e72f212ab333f899c1d95ed4e784131a3
Andre Castellanos <acastellanosaldama@gmail.com>
2021-01-12 23:21:37 -0600

    prettify Startup.cs

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -35,86 +35,93 @@ using WebAPI.Middleware;
 
 namespace WebAPI
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<CursosOnlineContext>(opt => {
-                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddOptions();
-            services.Configure<ConnectionConfig>(Configuration.GetSection("ConnectionStrings"));
-            services.AddMediatR(typeof(Consulta.ListaCursosHandler).Assembly);
-            services.AddControllers(opt => 
-                {
-                    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-                    opt.Filters.Add(new AuthorizeFilter(policy));
-                }).
-                AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
-
-            var builder = services.AddIdentityCore<User>();
-            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-
-            identityBuilder.AddRoles<IdentityRole>();
-            identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<User, IdentityRole>>();
-            identityBuilder.AddEntityFrameworkStores<CursosOnlineContext>();
-            identityBuilder.AddSignInManager<SignInManager<User>>();
-            services.TryAddSingleton<ISystemClock, SystemClock>();
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => 
-            {
-                opt.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = key,
-                    ValidateAudience = false,
-                    ValidateIssuer = false
-                };
-            });
-            services.AddScoped<IJwtGenerator, JwtGenerator>();
-            services.AddScoped<IUserSession, UserSession>();
-            services.AddAutoMapper(typeof(Consulta.ListaCursosHandler));
-            services.AddTransient<IFactoryConnection, FactoryConnection>();
-            services.AddScoped<IInstructor, InstructorRepositorio>();
-            services.AddScoped<IPaginacion, PaginacionRepositorio>();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Servicios para mantenimiento de cursos", Version = "v1" });
-                c.CustomSchemaIds(c => c.FullName);
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                // app.UseDeveloperExceptionPage();
-                app.UseMiddleware<ErrorHandlingMiddleware>();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CursosOnline v1"));
-            }
-
-            app.UseAuthentication();
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddCors(o => o.AddPolicy("corsApp", builder => 
+      {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+      }));
+      services.AddDbContext<CursosOnlineContext>(opt =>
+      {
+        opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+      });
+      services.AddOptions();
+      services.Configure<ConnectionConfig>(Configuration.GetSection("ConnectionStrings"));
+      services.AddMediatR(typeof(Consulta.ListaCursosHandler).Assembly);
+      services.AddControllers(opt =>
+          {
+            var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            opt.Filters.Add(new AuthorizeFilter(policy));
+          }).
+          AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
+
+      var builder = services.AddIdentityCore<User>();
+      var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+
+      identityBuilder.AddRoles<IdentityRole>();
+      identityBuilder.AddClaimsPrincipalFactory<UserClaimsPrincipalFactory<User, IdentityRole>>();
+      identityBuilder.AddEntityFrameworkStores<CursosOnlineContext>();
+      identityBuilder.AddSignInManager<SignInManager<User>>();
+      services.TryAddSingleton<ISystemClock, SystemClock>();
+      var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+      {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+          ValidateIssuerSigningKey = true,
+          IssuerSigningKey = key,
+          ValidateAudience = false,
+          ValidateIssuer = false
+        };
+      });
+      services.AddScoped<IJwtGenerator, JwtGenerator>();
+      services.AddScoped<IUserSession, UserSession>();
+      services.AddAutoMapper(typeof(Consulta.ListaCursosHandler));
+      services.AddTransient<IFactoryConnection, FactoryConnection>();
+      services.AddScoped<IInstructor, InstructorRepositorio>();
+      services.AddScoped<IPaginacion, PaginacionRepositorio>();
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Servicios para mantenimiento de cursos", Version = "v1" });
+        c.CustomSchemaIds(c => c.FullName);
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        // app.UseDeveloperExceptionPage();
+        app.UseMiddleware<ErrorHandlingMiddleware>();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CursosOnline v1"));
+      }
+
+      app.UseCors("corsApp");
+
+      app.UseAuthentication();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
